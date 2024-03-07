59562326d023c5b435c32a10c4705e97b039b542
Thanh Vu <111718979+thanhtvu@users.noreply.github.com>
2024-03-06 20:26:50 -0800

    add categories

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/searchRestaurant', (req, res) => {
   const sdk = require('api')('@yelp-developers/v1.0#ab2ihuolt8pvwud');
 
   sdk.auth('Bearer 22GD387m4MP1qpro4t9r5SbkIcq2-bgIviUVvwbyam8aT2tiuzvCyI8U7SBCyJ4b3z6a-_-iqSJECKXGtOZ240EhUk1nl9WBFry9Z1eKlPwCCSHlLbHUysly7qXiZXYx');
-  sdk.v3_business_search({sort_by: 'best_match', latitude:'47.6238799', longitude:'-122.1328896'})
+  sdk.v3_business_search({categories: req.query.categories, sort_by: 'best_match', latitude:'47.6238799', longitude:'-122.1328896'})
   .then(({ data }) => res.json(data))
   .catch(err => console.error(err));
 
